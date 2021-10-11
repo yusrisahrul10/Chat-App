373036f5c30c89aa333e821c78e4544ca2aedbb3
Yusri Sahrul <yusri10sahrul@gmail.com>
2021-10-11 11:03:15 +0700

    Chat Screen Layout Design

---
+++ app/src/main/java/engineer/yusrisahrul/chatapp/activites/ChatActivity.kt
@@ -0,0 +1,12 @@
+package engineer.yusrisahrul.chatapp.activites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import engineer.yusrisahrul.chatapp.R
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
